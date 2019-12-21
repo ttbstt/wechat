49372fcc64f2df15b34ab89bf2eb5546e31b3a8d
ttbstt <1692749884@qq.com>
2019-12-21 15:03:03 +0800

    update

---
+++ home_application/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = (
+    url(r'^$', views.home),
+    url(r'^dev-guide/$', views.dev_guide),
+    url(r'^contact/$', views.contact),
+    url(r'^helloworld/$', views.helloworld),
+    
+)
